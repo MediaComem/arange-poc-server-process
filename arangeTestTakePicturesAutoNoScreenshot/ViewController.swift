47bafa512fc569c9a32e4a7afc8b7ff70f3ca89b
camel113 <adrienbigler@gmail.com>
2018-07-09 09:36:20 +0200

    Initial Commit

---
+++ arangeTestTakePicturesAutoNoScreenshot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  arangeTestTakePicturesAutoNoScreenshot
+//
+//  Created by Adrien Bigler on 09.07.18.
+//  Copyright © 2018 Adrien Bigler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
